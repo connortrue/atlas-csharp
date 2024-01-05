ac33c326811e0c403ffb0d56c2d84b15e00c51ff
connortrue <connor.true.94@gmail.com>
2024-01-05 13:06:48 -0600

    9 try

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0}\nCurrency: (1)", percent, currency);
+        Console.WriteLine("Percent: {0}\nCurrency: {1}", percent, currency);
 	}
 }
