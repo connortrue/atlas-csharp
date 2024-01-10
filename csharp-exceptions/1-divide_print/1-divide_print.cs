3c2e6926e433af5a6ab32fda175d9aaf72fb7664
connortrue <connor.true.94@gmail.com>
2024-01-10 14:49:49 -0600

    1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Int 
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine($"{a} / {b} = 0");
+        }
+        finally
+        {
+            Console.WriteLine();
+        }
+    }
+}
