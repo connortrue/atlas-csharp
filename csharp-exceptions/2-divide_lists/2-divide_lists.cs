a0069a72c597b92f55439e6caf5d70343080e062
connortrue <connor.true.94@gmail.com>
2024-01-10 15:01:10 -0600

    2

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> resultList = new List<int>();
+
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                int result = 0;
+
+                if (i < list1.Count && i < list2.Count)
+                {
+                    if (list2[i] != 0)
+                    {
+                        result = list1[i] / list2[i];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Out of range");
+                }
+
+                resultList.Add(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+
+        return resultList;
+    }
+}
