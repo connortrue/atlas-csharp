4fef6651b8e168a355011f2e0336276276037a89
connortrue <connor.true.94@gmail.com>
2024-01-10 19:16:06 -0600

    5

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health; // private field for health
+        private string name = "No name"; // private field for name with default value
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {self.name} / Total Health: {self.health}";
+        }
+    }
+}
