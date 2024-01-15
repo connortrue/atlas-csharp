63c3a02f393128f2f98a4b776270afe37da7924c
connortrue <connor.true.94@gmail.com>
2024-01-15 14:52:40 -0600

    4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            Console.Write(i);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return newList;
+    }
+}
