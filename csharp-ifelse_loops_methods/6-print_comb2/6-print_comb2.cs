4771a82aec492d869450e650b48ba66a9f771362
connortrue <connor.true.94@gmail.com>
2024-01-14 20:59:39 -0600

    6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (count == 0)
+                {
+                    Console.Write($"{i}{j:D2}");
+                }
+                else
+                {
+                    Console.Write($", {i}{j:D2}");
+                }
+                count++;
+            }
+        }
+        Console.WriteLine();
+    }
+}
