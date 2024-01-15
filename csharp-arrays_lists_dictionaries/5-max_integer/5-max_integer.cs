e737498a7ea0e06bc5c1879cc714f838b0ff61e9
connortrue <connor.true.94@gmail.com>
2024-01-15 14:56:11 -0600

    5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int max = myList[0];
+        foreach (var num in myList)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
+    }
+}
