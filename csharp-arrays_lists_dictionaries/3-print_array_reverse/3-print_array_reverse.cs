3c08c5978c019f79261b3c9f2fef007ab5124c97
connortrue <connor.true.94@gmail.com>
2024-01-15 14:50:01 -0600

    3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+                if (i > 0)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
