48afd3afbdd225f7f38849bf0edeeab31b5ab814
connortrue <connor.true.94@gmail.com>
2024-01-16 17:31:53 -0600

    try again

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -2,49 +2,50 @@ using System;
 
 public class Shape
 {
-    public void Area()
-    {
-        return 0;
-    }
+   public virtual int Area()
+   {
+       return 0;
+   }
 
-    public override string ToString()
-    {
-        return "Generic Shape";
-    }
+   public override string ToString()
+   {
+       return "Generic Shape";
+   }
 }
+
 public class Rectangle : Shape
 {
-    private int width;
-    private int height;
+   private int width;
+   private int height;
 
-    public int Width
-    {
-        get => width;
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentException("Width must be greater than or equal to 0");
-            }
-            width = value;
-        }
-    }
+   public int Width
+   {
+       get => width;
+       set
+       {
+           if (value < 0)
+           {
+               throw new ArgumentException("Width must be greater than or equal to 0");
+           }
+           width = value;
+       }
+   }
 
-    public int Height
-    {
-        get => height;
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentException("Height must be greater than or equal to 0");
-            }
-            height = value;
-        }
-    }
+   public int Height
+   {
+       get => height;
+       set
+       {
+           if (value < 0)
+           {
+               throw new ArgumentException("Height must be greater than or equal to 0");
+           }
+           height = value;
+       }
+   }
 
-    public void Area()
-    {
-        return width * height;
-    }
+   public override int Area()
+   {
+       return width * height;
+   }
 }
