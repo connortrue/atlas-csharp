2b8e647d4a9476d3d0138719a30949472fa30625
connortrue <connor.true.94@gmail.com>
2024-01-16 15:57:47 -0600

    notice the cap means we're calling the function not accessing the var

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,43 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+}
