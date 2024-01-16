87bf10b3bd016a459ed31edbe7ffe84e689a40ba
connortrue <connor.true.94@gmail.com>
2024-01-16 15:53:56 -0600

    6 why overrride

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    public override int Area()
+    {
+        return width * height;
+    }
+}
