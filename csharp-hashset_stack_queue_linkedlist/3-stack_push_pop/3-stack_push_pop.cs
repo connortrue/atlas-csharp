492a2390a3907e20c2ecdb506d4ad3a5414b2f1b
connortrue <connor.true.94@gmail.com>
2024-01-16 23:53:36 -0600

    use array 3

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,45 +1,38 @@
-using System;
-using System.Collections.Generic;
-
-class MyStack
+public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
 {
-    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
-    {
-        // Print the number of items in aStack
-        Console.WriteLine("Number of items: " + aStack.Count);
+   // Print the number of items in aStack
+   Console.WriteLine("Number of items: " + aStack.Count);
 
-        // Print the item at the top of aStack without removing it
-        if (aStack.Count > 0)
-        {
-            Console.WriteLine("Top item: " + aStack.Peek());
-        }
-        else
-        {
-            Console.WriteLine("Stack is empty");
-        }
+   // Print the item at the top of aStack without removing it
+   if (aStack.Count > 0)
+   {
+       Console.WriteLine("Top item: " + aStack.Peek());
+   }
+   else
+   {
+       Console.WriteLine("Stack is empty");
+   }
 
-        // Print if aStack contains a given item search
-        bool containsSearch = aStack.Contains(search);
-        Console.WriteLine("Stack contains \"" + search + "\": " + containsSearch);
+   // Print if aStack contains a given item search
+   bool containsSearch = aStack.Contains(search);
+   Console.WriteLine("Stack contains \"" + search + "\": " + containsSearch);
 
-        // If aStack contains the given item search, remove all items up to and including search; otherwise, leave aStack as is
-        Stack<string> tempStack = new Stack<string>();
-        while (aStack.Count > 0 && aStack.Peek() != search)
-        {
-            tempStack.Push(aStack.Pop());
-        }
-        if (aStack.Count > 0 && aStack.Peek() == search)
-        {
-            aStack.Pop(); // Remove the search item
-        }
-        while (tempStack.Count > 0)
-        {
-            aStack.Push(tempStack.Pop());
-        }
+   // Convert the stack into an array
+   string[] arr = aStack.ToArray();
 
-        // Add a new given item newItem to aStack
-        aStack.Push(newItem);
+   // Find the index of the search item in the array and remove all items from the index of the search item to the end of the array
+   int index = Array.IndexOf(arr, search);
+   if (index >= 0)
+   {
+       arr = arr.Take(index).ToArray();
+   }
 
-        return aStack;
-    }
+   // Add the new item to the array
+   List<string> list = new List<string>(arr);
+   list.Add(newItem);
+
+   // Convert the list back into a stack
+   Stack<string> result = new Stack<string>(list);
+
+   return result;
 }
