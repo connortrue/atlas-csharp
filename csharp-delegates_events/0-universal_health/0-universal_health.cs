44dc06c25e4411a7d9805010e30108257c31b54c
connortrue <connor.true.94@gmail.com>
2024-01-24 13:07:08 -0600

    0

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,24 @@
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name, float maxHp)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
+            maxHp = 100f;
+        }
+
+        this.name = name ?? "Player";
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
