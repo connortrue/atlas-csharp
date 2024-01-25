e78e7d882ca2f08a9092e5cbf919d1badcedf4e5
connortrue <connor.true.94@gmail.com>
2024-01-24 23:14:28 -0600

    0

--- csharp-text_based_interface/InventoryLibrary/Program.cs
+++ csharp-text_based_interface/InventoryLibrary/Program.cs
@@ -39,14 +39,14 @@ namespace InventoryManager
         {
             Console.WriteLine("Inventory Manager");
             Console.WriteLine("-------------------------");
-            Console.WriteLine("{ClassNames} show all ClassNames of objects");
-            Console.WriteLine("{All} show all objects");
-            Console.WriteLine("{All [ClassName]} show all objects of a ClassName");
-            Console.WriteLine("{Create [ClassName]} a new object");
-            Console.WriteLine("{Show [ClassName object_id]} an object");
-            Console.WriteLine("{Update [ClassName object_id]} an object");
-            Console.WriteLine("{Delete [ClassName object_id]} an object");
-            Console.WriteLine("{Exit}");
+            Console.WriteLine("<ClassNames> show all ClassNames of objects");
+            Console.WriteLine("<All> show all objects");
+            Console.WriteLine("<All [ClassName]> show all objects of a ClassName");
+            Console.WriteLine("<Create [ClassName]> a new object");
+            Console.WriteLine("<Show [ClassName object_id]> an object");
+            Console.WriteLine("<Update [ClassName object_id]> an object");
+            Console.WriteLine("<Delete [ClassName object_id]> an object");
+            Console.WriteLine("<Exit>");
         }
 
         static void PrintClassNames()
