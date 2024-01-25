9dd0284a52e802c8c8b3ce92f1657f95e133fb52
connortrue <connor.true.94@gmail.com>
2024-01-24 18:55:01 -0600

    5

--- csharp-delegates_events/5-eventful/5-eventful.cs
+++ csharp-delegates_events/5-eventful/5-eventful.cs
@@ -1,159 +1,55 @@
 using System;
 
-public enum Modifier
+public delegate void HPCheckDelegate(object sender, CurrentHPArgs e);
+
+public class CurrentHPArgs
 {
-    Weak,
-    Base,
-    Strong
+    public float currentHp { get; set; }
+    public float maxHp { get; set; }
 }
-
-public delegate float CalculateModifier(float baseValue, Modifier modifier);
-
-public class CurrentHPArgs : EventArgs
-{
-    public float CurrentHp { get; }
-
-    public CurrentHPArgs(float newHp)
-    {
-        CurrentHp = newHp;
-    }
-}
-
-
 
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
+    public event HPCheckDelegate HPCheck;
 
-    public Player(string name = "Player", float maxHp = 100f)
+    private void HPValueWarning(object sender, CurrentHPArgs e)
     {
-        if (maxHp <= 0)
+        if (e.currentHp == 0)
         {
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
-            maxHp = 100f;
-        }
-
-        this.name = name ?? "Player";
-        this.maxHp = maxHp;
-        this.hp = maxHp;
-        HPCheck += CheckStatus;
-    }
-
-    public void PrintHealth()
-    {
-        Console.WriteLine($"{name} has {hp} / {maxHp} health");
-    }
-
-    public float TakeDamage(float damage)
-    {
-        if (damage >= 0)
-        {
-            Console.WriteLine($"{name} takes {damage} damage!");
-            hp -= damage;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Health has reached zero!");
+            Console.ResetColor();
         }
         else
         {
-            Console.WriteLine($"{name} takes 0 damage!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Health is low!");
+            Console.ResetColor();
         }
-        ValidateHP(hp);
-        return hp;
     }
 
-    public float HealDamage(float heal)
+    public void OnCheckStatus(CurrentHPArgs e)
     {
-        if (heal >= 0)
+        if (e.currentHp < e.maxHp / 4)
         {
-            Console.WriteLine($"{name} heals {heal} HP!");
-            hp += heal;
+            Console.WriteLine($"Electric Mouse isn't doing too great...");
+            HPCheck += HPValueWarning;
+        }
+        else if (e.currentHp >= e.maxHp / 4 && e.currentHp < e.maxHp * 0.75)
+        {
+            Console.WriteLine($"Electric Mouse is doing well!");
+            HPCheck += HPValueWarning;
         }
         else
         {
-            Console.WriteLine($"{name} heals 0 HP!");
+            Console.WriteLine($"Electric Mouse is knocked out!");
+            HPCheck += HPValueWarning;
         }
-        ValidateHP(hp);
-        return hp;
+        HPCheck?.Invoke(this, e);
     }
 
     public void ValidateHP(CurrentHPArgs e)
     {
         OnCheckStatus(e);
     }
-
-
-    public float ApplyModifier(float baseValue, Modifier modifier)
-    {
-        switch (modifier)
-        {
-            case Modifier.Weak:
-                return baseValue * 0.5f;
-            case Modifier.Base:
-                return baseValue;
-            case Modifier.Strong:
-                return baseValue * 1.5f;
-            default:
-                throw new ArgumentException("Invalid modifier", nameof(modifier));
-        }
-    }
-
-    public event EventHandler<CurrentHPArgs> HPCheck;
-
-    private string status;
-
-    private void CheckStatus(object sender, CurrentHPArgs e)
-    {
-        float maxHp = 100f;
-        float halfMaxHp = maxHp / 2;
-        float quarterMaxHp = maxHp / 4;
-
-    if (e.CurrentHp == maxHp)
-    {
-        status = $"{name} is in perfect health!";
-    }
-    else if (e.CurrentHp >= halfMaxHp && e.CurrentHp <= maxHp)
-    {
-        status = $"{name} is doing well!";
-    }
-    else if (e.CurrentHp >= quarterMaxHp && e.CurrentHp <= halfMaxHp)
-    {
-        status = $"{name} isn't doing too great...";
-    }
-    else if (e.CurrentHp > 0 && e.CurrentHp <= quarterMaxHp)
-    {
-        status = $"{name} needs help!";
-    }
-    else if (e.CurrentHp == 0)
-    {
-        status = $"{name} is knocked out!";
-    }
-
-        Console.WriteLine(status);
-    }
-
-    private void HPValueWarning(object sender, CurrentHPArgs e)
-{
-    if (e.currentHp == 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Health has reached zero!");
-        Console.ResetColor();
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("Health is low!");
-        Console.ResetColor();
-    }
-}
-
-public void OnCheckStatus(CurrentHPArgs e)
-{
-    if (e.currentHp < e.maxHp / 4)
-    {
-        HPCheck += HPValueWarning;
-    }
-    HPCheck?.Invoke(this, e);
-}
-
 }
