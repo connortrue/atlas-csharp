682fc319a077e99369cdd7b0447ea14bbba03a33
connortrue <connor.true.94@gmail.com>
2024-01-24 20:39:14 -0600

    base

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string id { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_updated { get; set; }
+    }
+}
